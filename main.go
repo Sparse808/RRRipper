82322462e6da6f8697d94ba727c2e2719366a5e3
Sparse808 <sparse.dev@gmail.com>
2025-06-27 22:03:44 -0400

    Modified Ripper for Royal Road

--- main.go
+++ main.go
@@ -1,235 +1,130 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 	"log"
-	"golang.org/x/net/html"
-	"strings"
+	"net/http"
 	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+
 	//"io"
-	"os"
 	"bytes"
-	"time"
 	"math/rand"
+	"os"
+	"regexp"
+	"time"
 )
 
 type Chapter struct {
-	Name string
-	Link string
+	Name string `json:"title"`
+	Link string `json:"url"`
 }
 
 func main() {
 	fmt.Println("Starting Program")
 	rand.Seed(time.Now().UnixNano())
-	url := "https://www.lightnovelpub.com/novel/atticuss-odyssey-reincarnated-into-a-playground/chapters"
-	domain := strings.SplitAfter(url,".com")[0]
-	bookName := strings.Split(strings.SplitAfter(strings.SplitAfter(url,"novel/")[1],"/")[0],"/")[0]
+	url := "https://www.royalroad.com/fiction/63759/super-supportive"
+	domain := strings.SplitAfter(url, ".com")[0]
+	bookName := strings.SplitAfter(url, "/")[5]
 	fmt.Println("filename: " + bookName)
 
-	req, err := http.NewRequest("GET",url,nil)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Referer", "https://www.google.com")
-
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Panic(err)
-		return
-	} 
 
-	defer resp.Body.Close()
+	doc := requestPageHTML(url, client)
 
-	fmt.Println(resp.StatusCode)
-
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		log.Panic(err)
-		return
-	}
-
-	chapterPages := []*html.Node{doc}
 	allChapters := []Chapter{}
 
-	callPages(doc, domain, &chapterPages, client)
-	
-
-	fmt.Println(chapterPages)
-	//Gets every chapter page
-	for _, page := range chapterPages {
-		allChapters = append(allChapters, getChapters(page, domain)...)
-	}
+	callPages(doc, domain, &allChapters)
 
 	printStuff(allChapters)
-	
-	callChapter(allChapters, client,bookName)
-	
+
+	callChapter(allChapters, client, bookName)
+
 }
 
-func callChapter(allChapters []Chapter, client *http.Client, bookName string) {
-	fullHTML := ""
+func requestPageHTML(url string, client *http.Client) *html.Node {
+	var response http.Response
+	tryCount := 0
 
-	for index := 0; index < len(allChapters); index++ {
-	//for _, chapterPage := range allChapters {
-		fmt.Println(allChapters[index].Link)
-		pageReq, err := http.NewRequest("GET", allChapters[index].Link, nil)
+	for response.StatusCode != 200 {
+		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			log.Panic(err)
-			return
-		}
-		pageReq.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-		pageReq.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-		pageReq.Header.Set("Accept-Language", "en-US,en;q=0.5")
-		pageReq.Header.Set("Connection", "keep-alive")
-		pageReq.Header.Set("Referer", "https://www.google.com")
-
-		time.Sleep(time.Duration(rand.Intn(2000)+1000) * time.Millisecond)
-
-		pageResp, err2 := client.Do(pageReq)
-		if err2 != nil {
-			//log.Panic(err2)
-			//return
-			
-		}
-		// pageResp.Body.Close()
-
-		fmt.Println(pageResp.StatusCode)
-
-		for pageResp.StatusCode != 200 || err2 != nil{
-			pageReq2, err5 := http.NewRequest("GET", allChapters[index].Link, nil)
-		if err5 != nil {
-			log.Panic(err5)
-			return
-		}
-		pageReq2.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-		pageReq2.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-		pageReq2.Header.Set("Accept-Language", "en-US,en;q=0.5")
-		pageReq2.Header.Set("Connection", "keep-alive")
-		pageReq2.Header.Set("Referer", "https://www.google.com")
-
-		time.Sleep(time.Duration(rand.Intn(5000)+10000) * time.Millisecond)
-
-		pageResp2, err4 := client.Do(pageReq2)
-		if err4 != nil {
-			//log.Panic(err4)
-			//return
 		}
 
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+		req.Header.Set("Connection", "keep-alive")
+		req.Header.Set("Referer", "https://www.google.com")
 
-		pageResp = pageResp2
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatal("Can not connect to url")
 		}
-		
+		defer resp.Body.Close()
 
-		tempDoc, err3 := html.Parse(pageResp.Body)
-		if err3 != nil {
-			log.Panic(err3)
-			return
-		}
-		//fmt.Println(tempDoc)
+		fmt.Println("Status Code: ", resp.StatusCode)
 
-		var chapContent *html.Node
-
-		for node := range tempDoc.Descendants(){
-			if node.Type != html.ElementNode || node.Data != "div" {
-				continue
+		if resp.StatusCode != 200 {
+			if tryCount > 5 {
+				log.Fatal("Cant make 200 connection")
 			}
-			for _, ele := range node.Attr {
-				if ele.Key != "id"  || ele.Val != "chapter-container"{
-					continue
+			tryCount++
+			continue
+		}
+
+		response = *resp
+	}
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		log.Fatal("Failed parsing html")
+	}
+
+	return doc
+
+}
+
+// Gets us all the pages that have 100 chapters each
+func callPages(doc *html.Node, domain string, allChapters *[]Chapter) {
+	for item := range doc.Descendants() {
+		if item.Type == html.TextNode {
+			if len(item.Data) > 27 {
+				if strings.TrimSpace(item.Data[:27]) == "window.fiction" {
+
+					re := regexp.MustCompile(`window\.chapters\s*=\s*(\[[\s\S]*?\]);`)
+
+					jsons := re.FindStringSubmatch(item.Data)
+
+					if len(jsons) < 2 {
+						log.Fatal("json chapters not found")
+					}
+
+					jsonArray := jsons[1]
+
+					err := json.Unmarshal([]byte(jsonArray), &allChapters)
+
+					if err != nil {
+						log.Fatal("error parsing chapters json")
+					}
+
 				}
-				
-				node.PrevSibling = nil
-				node.NextSibling = nil
-				fmt.Println(node)
-				chapContent = node
-				break
-			} 
-			
+			}
+
 		}
 
-		var buf bytes.Buffer
-		if err := html.Render(&buf, chapContent); err != nil {
-			log.Fatal(err)
-		}
-
-		partHTML := "<h2>" + allChapters[index].Name +"</h2>" + buf.String() 
-		fullHTML = fullHTML + partHTML
 	}
 
-
-	finishedHTML :="<!DOCTYPE html>\n<html>\n<head><meta charset=\"UTF-8\"></head>\n<body>\n" + fullHTML + "\n</body>\n</html>"
-	//fmt.Println(finishedHTML)
-	os.WriteFile(bookName + ".html", []byte(finishedHTML), 0644)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func callPages(doc *html.Node, domain string, chapterPages *[]*html.Node, client *http.Client) {
-	//Gets us all the pages that have 100 chapters each
-	for _, fullPage := range getTotalPages(doc, domain) {
-		fmt.Println(fullPage)
-		pageReq, err := http.NewRequest("GET", fullPage, nil)
-		if err != nil {
-			log.Panic(err)
-			return
-		}
-		pageReq.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-		pageReq.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-		pageReq.Header.Set("Accept-Language", "en-US,en;q=0.5")
-		pageReq.Header.Set("Connection", "keep-alive")
-		pageReq.Header.Set("Referer", "https://www.google.com")
-
-		time.Sleep(time.Duration(rand.Intn(2000)+1000) * time.Millisecond)
-
-		pageResp, err2 := client.Do(pageReq)
-		if err2 != nil {
-			log.Panic(err2)
-			return
-		}
-		
-		//defer pageReq.Body.Close()
-
-		fmt.Println(pageResp.StatusCode)
-
-		tempDoc, err3 := html.Parse(pageResp.Body)
-		if err3 != nil {
-			log.Panic(err3)
-			return
-		}
-		*chapterPages = append(*chapterPages, tempDoc)
-
+	for index, item := range *allChapters {
+		(*allChapters)[index].Link = domain + item.Link
 	}
+
 }
-
-
 
 func getTotalPages(doc *html.Node, domain string) []string {
 	halfURL := ""
@@ -244,7 +139,7 @@ func getTotalPages(doc *html.Node, domain string) []string {
 			}
 			if ele.Val == "pagination" {
 				//inside of page navigation list
-				for lists := range node.Descendants(){
+				for lists := range node.Descendants() {
 					//fmt.Println(lists)
 					if lists.Type != html.ElementNode || lists.Data != "li" {
 						continue
@@ -255,7 +150,7 @@ func getTotalPages(doc *html.Node, domain string) []string {
 						lastPage := lists.FirstChild
 						for _, attribute := range lastPage.Attr {
 							if attribute.Key == "href" {
-								split := strings.SplitAfter(attribute.Val,"=")
+								split := strings.SplitAfter(attribute.Val, "=")
 								halfURL = split[0]
 								tempNumber, _ := strconv.Atoi(split[1])
 								lastPageNumber = tempNumber
@@ -270,52 +165,52 @@ func getTotalPages(doc *html.Node, domain string) []string {
 
 	pageURLs := []string{}
 	for index := 2; index <= lastPageNumber; index++ {
-		pageURLs = append(pageURLs, domain + halfURL +strconv.Itoa(index))
+		pageURLs = append(pageURLs, domain+halfURL+strconv.Itoa(index))
 	}
 
 	return pageURLs
 }
 
+func callChapter(allChapters []Chapter, client *http.Client, bookName string) {
+	fullHTML := ""
 
-func getChapters(doc *html.Node, domain string) []Chapter{
-	chapters := []Chapter{}
+	for index := 0; index < len(allChapters); index++ {
 
-	for node := range doc.Descendants() {
-		if node.Type != html.ElementNode || node.Data != "ul"{
-			continue
-		}
-		for _, att := range node.Attr {
-			if att.Key != "class" || att.Val != "chapter-list" {
+		tempDoc := requestPageHTML(allChapters[index].Link, client)
+
+		var chapContent *html.Node
+
+		for node := range tempDoc.Descendants() {
+			if node.Type != html.ElementNode || node.Data != "div" {
 				continue
 			}
-			//fmt.Println("Chapter List Node")
-			//fmt.Println(node)
-			for nodes := range node.Descendants() {
-				if nodes.Type != html.ElementNode || nodes.Data != "a"{ 
+			for _, ele := range node.Attr {
+				if ele.Key != "class" || ele.Val != "chapter-inner chapter-content" {
 					continue
 				}
-				tempChapter := Chapter{}
-				for _, attribute := range nodes.Attr {
-					if attribute.Key == "title" {
-						//fmt.Println(attribute.Val)
-						tempChapter.Name = attribute.Val
-					
-					}
-					if attribute.Key == "href" {
-						//fmt.Println(attribute.Val)
-						tempChapter.Link = domain + attribute.Val
-					}
-				}
-				//fmt.Println(tempChapter)
-				chapters = append(chapters, tempChapter)
-			}
-		}
-	}
-	//fmt.Println(chapters)
-	//fmt.Println(doc)
-	return chapters
-}
 
+				node.PrevSibling = nil
+				node.NextSibling = nil
+				fmt.Println(node)
+				chapContent = node
+				break
+			}
+
+		}
+
+		var buf bytes.Buffer
+		if err := html.Render(&buf, chapContent); err != nil {
+			log.Fatal(err)
+		}
+
+		partHTML := "<h2>" + allChapters[index].Name + "</h2>" + buf.String()
+		fullHTML = fullHTML + partHTML
+	}
+
+	finishedHTML := "<!DOCTYPE html>\n<html>\n<head><meta charset=\"UTF-8\"></head>\n<body>\n" + fullHTML + "\n</body>\n</html>"
+	//fmt.Println(finishedHTML)
+	os.WriteFile(bookName+".html", []byte(finishedHTML), 0644)
+}
 
 func printStuff(chaps []Chapter) {
 	for _, chap := range chaps {
